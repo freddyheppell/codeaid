76ce3c42ff640e3c092ca5a1a7f989bbbab3111a
Freddy Heppell <f.heppell@gmail.com>
2015-07-29 12:46:01 +0000

    added auth query url

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -36,3 +36,7 @@ Route::get('auth/logout', 'Auth\AuthController@getLogout');
 // Registration routes...
 Route::get('auth/register', 'Auth\AuthController@getRegister');
 Route::post('auth/register', 'Auth\AuthController@postRegister');
+
+Route::get('/auth/status', function(){
+   return Auth::user();
+});
